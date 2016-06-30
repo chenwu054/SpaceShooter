281cc623c46179a940eda7b6281752ce1f6c733d
Chen Wu <chenwu054@Chens-MacBook-Pro.local>
2016-06-29 19:16:55 -0700

    added shot

---
+++ Assets/_Scripts/Mover.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+var speed : float;
+var rb : Rigidbody;
+function Start () {
+	rb = GetComponent.<Rigidbody>();
+	rb.velocity = transform.forward * speed;
+}
+
---
+++ Assets/_Scripts/PlayerController.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+class Boundary {
+	var xMin : float;
+	var xMax : float;
+	var yMin : float;
+	var yMax : float;
+}
+
+var speed : float = 1.0;
+var rb : Rigidbody; 
+var boundary : Boundary;
+var tilt : float = 1.0;
+var fireRate : float;
+private var nextFire : float = 0.0;
+var shot : GameObject;
+var shotSpawn : Transform;
+
+function Start () {
+// to get the reference to the rigidbody object
+	rb = GetComponent.<Rigidbody>();
+}
+function Update() {
+	
+	if(Input.GetButton("Fire1") && Time.time > nextFire) {
+		nextFire = Time.time + fireRate;
+		Instantiate(shot, shotSpawn.position, shotSpawn.rotation); // instantiate a new object
+	}
+}
+
+function FixedUpdate() {
+	var y : float = Input.GetAxis ("Vertical") * speed;
+	var x : float = Input.GetAxis ("Horizontal") * speed;
+	var movement : Vector3 = new Vector3(x, 0.0, y);
+	rb.velocity = movement * speed;
+	rb.position = Vector3(
+	Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+	0.0,
+	Mathf.Clamp(rb.position.z, boundary.yMin, boundary.yMax));
+	rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+
+}
+
+
